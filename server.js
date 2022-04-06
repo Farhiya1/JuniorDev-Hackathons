da0fd23b548d8013cf30d465dfdcd1aca6bd8482
cee breezy <djmisscbrown@hotmail.com>
2022-04-06 10:11:50 +0100

    fixed bug on server

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers'));
+// app.use(require('./controllers'));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
