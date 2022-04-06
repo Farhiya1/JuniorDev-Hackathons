8b7cb309ee93ce019fa713555a1181ad5a068c99
cee breezy <djmisscbrown@hotmail.com>
2022-04-06 10:12:11 +0100

    Added functionality for seeding db

---
+++ seeds/seeds.js
@@ -0,0 +1,21 @@
+const sequelize = require('../config/connection');
+const { User, Team, Project } = require('../models');
+
+const userData = require('./user.json');
+const projectData = require('./projects.json');
+const teamsData = require('./teams.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    for (const project of projectData) {
+        await Project.create({
+
+        })
+    }
+}
